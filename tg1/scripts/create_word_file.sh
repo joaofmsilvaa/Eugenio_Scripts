8ad8aee0252935f97c4acc6eef064e4ed68833ed
joaocalad0 <joaocalado33@gmail.com>
2024-10-22 15:16:00 +0100

    Fixed paths issue

--- tg1/scripts/create_word_file.sh
+++ tg1/scripts/create_word_file.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-CORPUS_FILE="../corpus_txt/corpus_clean.txt"
-OUTPUT_DIR="../corpus_txt/words_dict"
+CORPUS_FILE="../deu_mixed-typical_2011_300K-sentences.txt"
+OUTPUT_DIR="../words_dict"
 OUTPUT_FILE="$OUTPUT_DIR/words.txt"
 
-if [[ ! -f $CORPUS_FILE ]]; then
-    echo "O arquivo de corpus não foi encontrado!"
-    exit 1
-fi
 
 tr -c '[:alpha:]' '[\n*]' < "$CORPUS_FILE" |  
 tr '[:upper:]' '[:lower:]' |                
