5fd13d2612e38c6396be51eb45c415230669fcb1
João Silva <joaospamsilva@gmail.com>
2024-10-26 18:06:44 +0000

    Fixed phrase pairs script

--- tg1/scripts/create_phrase_pairs_file.sh
+++ tg1/scripts/create_phrase_pairs_file.sh
@@ -1,12 +1,9 @@
+CORPUS_FILE="../deu_mixed-typical_2011_300K-sentences.txt"              
+OUTPUT_DIR="../sentences_dict"
+OUTPUT_FILE="$OUTPUT_DIR/sentences_pairs.txt"
 
-CORPUS_FILE="../deu_mixed-typical_2011_300K-sentences.txt"
-OUTPUT_FILE="../sentences_dict/sentences_pairs.txt"
-
-# cat - read the entire file
-# tr - replace spaces with the "|" sign
-# awk - filter the output to remove the line number
-# sed - concatenate the odd line ($) with the even line (N)
-# sort - sort the output by alphabethic order
-# uniq -c - count the occurrences of each line
-cat $CORPUS_FILE | tr ' ' '|' | awk '{print $2}' | uniq -c | sed '$!N;s/\n//' | sed 's/  */ /g'| sort > $OUTPUT_FILE
-
+awk -F'\t' '{print $2}' "$CORPUS_FILE" |
+sed 's/ /|/g' | sort -u |
+tee >(tail -n +2) | paste -d "" - - |
+sort | uniq -c |
+awk '{print $1 " " $2}' > "$OUTPUT_FILE"
