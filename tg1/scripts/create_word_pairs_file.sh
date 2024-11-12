018307e3e816f380b89494081ac57787754861cf
joaofmsilvaa <joaospamsilva@gmail.com>
2024-11-12 12:50:57 +0000

    Changed output structure
    Changed the output structure of the script to correspond to the files used in Eugenio V3

--- tg1/scripts/create_word_pairs_file.sh
+++ tg1/scripts/create_word_pairs_file.sh
@@ -10,10 +10,8 @@ awk 'NR==1 {prev=$0; next} {print prev "|" $0; prev=$0}' | # Joins the first wor
 sort |                                          # Sorts the pairs alphabetically.
 uniq -c |                                       # Counts the occurrence.
 sort -k2,2 |                                     # Sorts the lines by the pair.
-awk '{print $1 "\t" $2}' > "$OUTPUT_FILE"
+awk '{print $2 "\t" $1}' > "$OUTPUT_FILE"
 
-# Execute the limit_files.sh script (optional step)
 ./limit_files.sh $OUTPUT_FILE
 
-# Notificação de conclusão.
 echo "O Dicionário foi criado em $OUTPUT_FILE"
